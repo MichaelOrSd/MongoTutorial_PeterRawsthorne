ea1fe39a2a10414ded12b4daba2d4e0d187b5a41
Michael O'Reilly <86013664+MichaelOrSd@users.noreply.github.com>
2022-04-13 12:36:51 -0230

    all

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const { MongoCliient } = require('mongodb');
 const express = require('express');
 const app = express();
 
-const PORT = precess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const uri = "mongodb://localhost:27017/";
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index', {name: 'Peter'});
 });
 
-const searchRouter = require('./routes/awards');
+// const searchRouter = require('./routes/awards');
 
-app.use('/search', searchRouter);
+// app.use('/search', searchRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
